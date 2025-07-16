57b068b2275d03a3e9fc77e86ba40cef64d30cb1
esthedebeste <57283066+esthedebeste@users.noreply.github.com>
2025-07-16 20:55:43 +0200

    Make required pegs give more score

--- src/objects/peg.cpp
+++ src/objects/peg.cpp
@@ -7,9 +7,13 @@ Peg::Peg(Level *level, double x, double y)
   : CircleObject(level, x, y, &peg_img) { }
 
 void Peg::hit() {
-  if (!m_hit)
-    level()->increase_score(10, Vec2D{x() + radius() / 2, cy() + radius()});
+  if (m_hit)
+    return;
   m_hit = true;
+  if (m_required)
+    level()->increase_score(100, Vec2D{x(), cy() + radius()});
+  else
+    level()->increase_score(10, Vec2D{x() + radius() / 2, cy() + radius()});
 }
 
 void Peg::draw() {
